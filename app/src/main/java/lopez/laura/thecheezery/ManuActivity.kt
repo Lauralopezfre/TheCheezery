82e86347e4fc1a463ff44fc1f4f50078c276163d
Lauralopezfre <15325051520023@cetis108.edu.mx>
2021-02-19 02:49:58 -0700

    Finalización de la actividad

---
+++ app/src/main/java/lopez/laura/thecheezery/ManuActivity.kt
@@ -0,0 +1,11 @@
+package lopez.laura.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manu)
+    }
+}
